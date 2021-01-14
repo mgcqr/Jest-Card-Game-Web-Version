b45d3da033e1a73c9f903c7b18b74a0d4d46e48f
mgcqr <wuyufeii3e@163.com>
2021-01-14 15:53:19 +0800

    1st step

---
+++ jest-card-game/src/main/java/com/mgcqr/App.java
@@ -0,0 +1,13 @@
+package com.mgcqr;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
